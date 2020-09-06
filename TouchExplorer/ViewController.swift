257727da0cea81478a16768c868c0d20cc4aa2c6
blade8323 <blade8323@mail.ru>
2020-09-06 12:24:05 +0500

    Initial Commit

---
+++ TouchExplorer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchExplorer
+//
+//  Created by Владислав Соколов on 06.09.2020.
+//  Copyright © 2020 Владислав Соколов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
